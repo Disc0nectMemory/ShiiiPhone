298bef9897266a0a439c7b402a383e11873ce01a
Q1uvg <csqiiing.45@gmail.com>
2026-03-20 15:40:09 +0800

    改造成pwa项目

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,18 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import {registerSW} from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+  immediate: true,
+  onNeedRefresh() {
+    // Optional: show UI to let the user refresh
+    console.log('New content available, please refresh.');
+  },
+  onOfflineReady() {
+    console.log('App is ready to work offline.');
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
